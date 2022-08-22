ca020f4626a5c2d5c64855dd160d5d8ad13085c4
Alessio Cassieri <44462527+Cassio7@users.noreply.github.com>
2022-08-22 11:49:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 from telebot import types
 import sqlite3
 
-API_TOKEN = '5510728878:AAHhIitKWRAkv_A2xKH4ecGbzxzNuux7WDk'
+API_TOKEN = '5444107099:AAGKgArvJZioPb4dSL-TaZoQ7pVSlQwflNo'
 
 bot = telebot.TeleBot(API_TOKEN)
 conn = sqlite3.connect('database_progetto.db',check_same_thread=False)
@@ -24,20 +24,26 @@ id_cancella = ["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c
 def start(message):
     global chat_id
     chat_id = message.chat.id
-    bot.send_message(chat_id,'Inserisci matricola, 6 numeri')
+    bot.send_message(chat_id,'Inserisci matricola (6 cifre)')
     global flag
     flag[message.chat.id] = 0
+
+@bot.message_handler(commands=['info'])
+def info(message):
+    bot.send_message(message.chat.id,"Questo bot permette la prenotazione alle lezioni di Informatica dell'Università"
+                    +" degli studi di Perugia, tramite login con apposita matricola e password, premi /start per iniziare")
+
 
 def db_check_mat(matricola: int,message):
     cursor.execute("SELECT COUNT(password) FROM studenti WHERE matricola LIKE ?",(matricola[message.chat.id],))
     if cursor.fetchone()[0] == 1:
         global flag
         flag[chat_id] = 1
-        bot.send_message(message.chat.id,'Matricola trovata! Inserire password: ')
+        bot.reply_to(message,'Matricola trovata!\nInserire password: ')
     else:
-        bot.send_message(message.chat.id,'Matricola errata, riprova')
+        bot.reply_to(message,'Matricola errata, riprova')
 
-def db_check_psw(matricola: int,psw,message):
+def db_check_psw(matricola: int,psw,message): #rimuovere password inserita storico chat
     cursor.execute("SELECT password FROM studenti WHERE matricola LIKE ?",(matricola[message.chat.id],))
     if psw == cursor.fetchone()[0]:
         msg = bot.send_message(message.chat.id,'Password corretta! Benvenuto '+matricola[chat_id])
@@ -46,22 +52,40 @@ def db_check_psw(matricola: int,psw,message):
         homepage(message)
     else:
         bot.send_message(message.chat.id,'Password errata')
+    bot.delete_message(message.chat.id,message.id)
+
 
 @bot.message_handler(content_types = ["text"])
 def login(message):
-    if flag[chat_id] == 0:
-        if len(message.text) == 6 and message.text.isnumeric():
-            bot.reply_to(message, "Matricola inserita: "+message.text)
-            global matricola
-            matricola[chat_id] = message.text
-            db_check_mat(matricola,message)
+    try:
+        if flag[chat_id] == 0:
+            if len(message.text) == 6 and message.text.isnumeric():
+                global matricola
+                matricola[chat_id] = message.text
+                db_check_mat(matricola,message)
+            else:
+                bot.reply_to(message, "La matricola che hai inserito non è di 6 numeri")
+        elif logged == 0:
+            leng = False
+            num = False
+            upp = False
+            if len(message.text)>=6:
+                leng = True
+            for i in message.text:
+                if i.isnumeric():
+                    num = True
+                if i.isupper():
+                    upp = True
+            if leng and num and upp:
+                db_check_psw(matricola,message.text,message)
+            else:
+                bot.reply_to(message, "La password essere più lunga di 6 caratteri, deve avere 1 numero e 1 lettera grande")
+                bot.delete_message(message.chat.id,message.id)
         else:
-            bot.reply_to(message, "La matricola che hai inserito non è di 6 numeri")
-    elif logged == 0:
-        db_check_psw(matricola,message.text,message)
-    else:
-        bot.reply_to(message, "Utilizza soltanto i bottoni")
-        homepage(message)
+            bot.reply_to(message, "Utilizza soltanto i comandi")
+            homepage(message)
+    except:
+        bot.reply_to(message, "Per prima cosa premi /start per iniziare")
 
 
 def homepage(message):
@@ -73,23 +97,23 @@ def homepage(message):
     bot.send_message(chat_id,"Questa è la tua homepage "+str(matricola[chat_id])+" seleziona un comando: ",reply_markup=markup_home)
 
 @bot.callback_query_handler(func=lambda query: query.data == "lezioni")
-def scelta_lezioni(message):
+def scelta_lezioni(call):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 2
     markup.add(types.InlineKeyboardButton("Corsi del primo anno", callback_data="primo"),
                             types.InlineKeyboardButton("Corsi del secondo anno", callback_data="secondo"))
     markup.add(types.InlineKeyboardButton("Torna indietro", callback_data="indietro_home"))
-    bot.send_message(chat_id,"Scegli quali corsi visualizzare: ",reply_markup=markup)
+    bot.send_message(chat_id,"Scegli l'anno dei corsi disponibili: ",reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda query: query.data == "primo")
 def lista_lezioni_primo(call):
     cursor.execute('SELECT descrizione,data_ora FROM lezioni WHERE aula LIKE "I1"')
-    roba = cursor.fetchall()
+    query = cursor.fetchall()
     markup_primo = types.InlineKeyboardMarkup()
     markup_primo.row_width = 1
     a = 1
-    for i in roba:
+    for i in query:
         markup_primo.add(types.InlineKeyboardButton(i[0]+" "+i[1],callback_data=a))
         a = a + 1
     markup_primo.add(types.InlineKeyboardButton("Torna indietro",callback_data="indietro"))
@@ -99,11 +123,11 @@ def lista_lezioni_primo(call):
 @bot.callback_query_handler(func=lambda call: call.data == "secondo")
 def lista_lezioni_secondo(call):
     cursor.execute('SELECT descrizione,data_ora FROM lezioni WHERE aula LIKE "A0"')
-    roba = cursor.fetchall()
+    query = cursor.fetchall()
     markup_secondo = types.InlineKeyboardMarkup()
     markup_secondo.row_width = 1
     a = 12
-    for i in roba:
+    for i in query:
         markup_secondo.add(types.InlineKeyboardButton(i[0]+" "+i[1],callback_data=a))
         a = a + 1
     markup_secondo.add(types.InlineKeyboardButton("Torna indietro",callback_data="indietro"))
@@ -126,13 +150,13 @@ def indietro3(call):
     lista_lezioni_secondo(call)
 
 
-@bot.callback_query_handler(func=lambda call: call.data in id_lez)#gestire tutti i numeri non solo 1 per prenotazione
+@bot.callback_query_handler(func=lambda call: call.data in id_lez)
 def lezione(call):
     cursor.execute('SELECT * FROM lezioni WHERE id LIKE ?',(call.data,))
-    roba = cursor.fetchall()
+    query = cursor.fetchall()
     global id_lez_corrente
     id_lez_corrente = call.data
-    lez_info(roba)
+    lez_info(query)
     markup_lez = types.InlineKeyboardMarkup()
     markup_lez.row_width = 2
     if int(call.data) > 11:
@@ -146,17 +170,18 @@ def lezione(call):
         markup_lez.add(types.InlineKeyboardButton("Cancella prenotazione",callback_data="c"+id_lez_corrente))#gestire!!!!!!!!
     bot.send_message(chat_id,"Scegli: ",reply_markup=markup_lez)
 
-def lez_info(roba):
-    bot.send_message(chat_id,"Corso: "+str(roba[0][1])+"\nData e ora inizio: "+str(roba[0][2])+"\nAula: "+str(roba[0][3])+"\nDocente: "+str(roba[0][4])+"\nPosti disponibili: "+str(roba[0][5]))
+def lez_info(query):
+    print(type(query))
+    bot.send_message(chat_id,"Corso: "+str(query[0][1])+"\nData e ora inizio: "+str(query[0][2])+"\nAula: "+str(query[0][3])+"\nDocente: "+str(query[0][4])+"\nPosti disponibili: "+str(query[0][5]))
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "prenotazione")
-def prenotazione(call):
+def prenotazioni_disponibili(call):
     markup_pren = types.InlineKeyboardMarkup()
     cursor.execute('SELECT posto FROM prenotazioni WHERE id_lezione LIKE ?',(id_lez_corrente,))
-    roba = cursor.fetchall()
+    query = cursor.fetchall()
     postii = []
-    for i in roba:
+    for i in query:
         postii.append(i[0])
     for i in range(1,21):
         if i not in postii:
@@ -177,25 +202,27 @@ def nuova_prenotazione(call):
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "prenotazioni")
-def prenotazioni(call):
+def prenotazioni_effettuate(call):
     cursor.execute('SELECT * FROM prenotazioni WHERE matricola LIKE ? ',(matricola[chat_id],))
-    roba = cursor.fetchall()
+    query = cursor.fetchall()
     markup_canc = {}
     a = 0
-    for i in roba:
+    for i in query:
         cursor.execute('SELECT * FROM lezioni WHERE id LIKE ?',(i[2],))
-        info = cursor.fetchall()
-        lez_info(info)
+        query = cursor.fetchall()
+        lez_info(query)
         bot.send_message(chat_id,"Il tuo posto prenotato è il numero: "+str(i[3]))
         markup_canc[a] = types.InlineKeyboardMarkup()
         markup_canc[a].row_width = 1
         markup_canc[a].add(types.InlineKeyboardButton("Cancella prenotazione",callback_data="c"+str(i[2])))#GESTIONE DA FARE
-        bot.send_message(chat_id,"-",reply_markup=markup_canc[a])
+        bot.send_message(chat_id,"Comandi: ",reply_markup=markup_canc[a])
         a = a + 1
+    if a == 0:
+        bot.send_message(chat_id,"Non hai nessuna prenotazione")
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
     markup.add(types.InlineKeyboardButton("Torna indietro",callback_data="indietro_home"))
-    bot.send_message(chat_id,"-",reply_markup=markup)
+    bot.send_message(chat_id,"Comandi: ",reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data in id_cancella)
 def cancella(call):
